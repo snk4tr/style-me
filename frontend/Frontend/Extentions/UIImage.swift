8d47bb19d7d261e95daa61ab8a3a9af1385df06a
Sergey Kastryulin <snk4tr@gmail.com>
2019-02-17 23:34:41 +0300

    feat(frontend): person detection code (no model yet)

--- frontend/Frontend/Extentions/UIImage.swift
+++ frontend/Frontend/Extentions/UIImage.swift
@@ -16,13 +16,60 @@ extension UIImage {
         
         UIGraphicsBeginImageContextWithOptions(size, false, scale)
         draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-        let normalizedImage = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
+        let normalizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
         
-        return normalizedImage ?? self;
+        return normalizedImage ?? self
     }
     
     func containsPerson() -> Bool {
-        return true
+        let inception :InceptionV3 = InceptionV3()
+        
+        let resizedImage = self.resizeTo(size: CGSize(width: 299, height: 299))
+        let buffer = resizedImage.toBuffer()
+        
+        let prediction = try! inception.prediction(input: InceptionV3Input(image: buffer!))
+        
+        print("prediction.classLabel.lowercased(): \(prediction.classLabel.lowercased())")
+        if prediction.classLabel.lowercased() == "person" {
+            return true
+        }
+        
+        return false
+    }
+    
+    func resizeTo(size :CGSize) -> UIImage {
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        self.draw(in: CGRect(origin: CGPoint.zero, size: size))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return resizedImage
+    }
+    
+    func toBuffer() -> CVPixelBuffer? {
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(self.size.width), Int(self.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(self.size.width), height: Int(self.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        context?.translateBy(x: 0, y: self.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer
     }
 }
